2b69efa8a48bbe4349e3f0bd90ff6d4499d5cc1b
shakilmahammud <hshakil987@gmail.com>
2020-09-12 00:50:15 +0600

    2 Component Added

---
+++ src/Component/FeedPost/FeedPost.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './FeedPost.css';
+import {Avatar} from '@material-ui/core';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
+
+const FeedPost = ({profilePic,image,username,timestamp,message}) => {
+    return (
+        <div className="post">
+            <div className="post_top">
+                <Avatar
+                src={profilePic} className="post_Avatar"
+                />
+                <div className="post_topInfo">
+                <h3>{username}</h3>
+                <p>Timestamp....</p>
+                </div>
+            </div>
+            <div className="post_bottom">
+            <p>{message}</p>
+            </div>
+            <div className="post_image">
+                <img src={image} alt=""/>
+            </div>
+            <div className="post_options">
+                <div className="post_option">
+                    <ThumbUpIcon/>
+                    <p>Like</p>
+                </div>
+                <div className="post_option">
+                    <ChatBubbleOutlineIcon/>
+                    <p>Comment</p>
+                </div>
+                <div className="post_option">
+                    <NearMeIcon />
+                    <p>Share</p>
+                </div>
+                <div className="post_option">
+                    <AccountCircleIcon/>
+                    <ExpandMoreOutlinedIcon />
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default FeedPost;
